18758f8f4c2c5ea4f4f6384fbf7d5756402f5199
Lander Guirles <lander.guirles@opendeusto.es>
2025-09-24 19:39:43 +0200

    He creado un logo, y he creado la pagina de inicio de sesión que si le das a iniciar sesion te lleva a la ventana de borja

--- DeustoFlix/src/main/Main.java
+++ DeustoFlix/src/main/Main.java
@@ -1,12 +1,15 @@
 package main;
 
 import gui.MainGuiWindow;
+import gui.VentanaInicio;
 
 // Main para inicializar la aplicacion
 
 public class Main {
     public static void main(String[] args) {
         MainGuiWindow window = new MainGuiWindow();
-        window.setVisible(true);
+        VentanaInicio inicio = new VentanaInicio();
+        inicio.setVisible(true);
+      //  window.setVisible(true);
     }
 }
