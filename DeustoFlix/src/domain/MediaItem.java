f507966624fe01e3499770554626d51f0638581c
Borja Hernández <borja.hernandez@opendeusto.es>
2025-09-24 17:53:36 +0200

    chorradas

--- DeustoFlix/src/domain/MediaItem.java
+++ DeustoFlix/src/domain/MediaItem.java
@@ -17,10 +17,20 @@ public abstract class MediaItem {
         this.imagen = imagen;
     }
 
-    public String getTitulo() { return titulo; }
-    public String getDescripcion() { return descripcion; }
-    public Genero getGenero() { return genero; }
-    public Categoria getCategoria() { return categoria; }
-    public ImageIcon getImagen() { return imagen; }
+    public String getTitulo() {
+    	return titulo;
+    }
+    public String getDescripcion() {
+    	return descripcion;
+    }
+    public Genero getGenero() {
+    	return genero; 
+    }
+    public Categoria getCategoria() { 
+    	return categoria; 
+    }
+    public ImageIcon getImagen() { 
+    	return imagen; 
+    }
     public abstract String getTipo();
 }
